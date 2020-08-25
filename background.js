adcbcd8c8009abf5c9a68cca9e34bf12dc663f02
caleb park <calebparky@gmail.com>
2020-08-24 21:52:20 -0500

    1.2

--- background.js
+++ background.js
@@ -1,23 +1,27 @@
 var minuteInMs = 60000;
-var minutesInAdvance = 5; // implement changeable
+var minutesInAdvance = 5; // implement changeable with chrome storage
 
 var secondsOffset = (new Date()).getSeconds();
 setTimeout(startHandle, minuteInMs - secondsOffset * 1000);
 
 function startHandle() {
     handleTiming();
-    console.log((minuteInMs/1000 - secondsOffset) + " elapsed");
+    console.log((minuteInMs/1000 - secondsOffset) + " seconds elapsed");
     setInterval(handleTiming, minuteInMs);
 }
 
+function formatTime(timeObject) {
+    return timeObject.getDay() + ":" + (timeObject.getHours()/10==0 ? "0" : "") + timeObject.getHours() + ":" + (timeObject.getMinutes()/10==0 ? "0" : "") + timeObject.getMinutes(); 
+}
+
 function handleTiming() {
-    chrome.notifications.clear("remind-notif");
-    chrome.notifications.clear("join-notif");
+    //chrome.notifications.clear("remind-notif");
+    //chrome.notifications.clear("join-notif");
 
     var now = new Date();
-    var nowTime = now.getDay() + ":" + now.getHours() + ":" + now.getMinutes(); 
+    var nowTime = formatTime(now);
     var future = new Date(now.getTime() + minutesInAdvance * minuteInMs); // 5 minutes later
-    var futureTime = future.getDay() + ":" + future.getHours() + ":" + future.getMinutes(); // + ":" + now.getSeconds();
+    var futureTime = formatTime(future); // + ":" + now.getSeconds();
     console.log("now: " + nowTime + ", future: " + futureTime);
 
     chrome.storage.sync.get({'classList': {}}, function(classes) {
@@ -44,27 +48,29 @@ function handleTiming() {
                 url += password ? `&pwd=${password}` : '';
             }
 
-
             // early notif
             for (var i = 0; i < schedule.length; i++) {
+                // console.log(schedule[i]);
                 // if reminders are enabled
                 if (meeting.remind && futureTime==schedule[i]) {
                     var notifOptions = {
                         type:"basic",
                         iconUrl:"icons/icon48.png",
                         title:"OK Zoomer | Meeting Reminder",
-                        message:`"${meetingName} is starting in ${minutesInAdvance} minutes!"`,
+                        message:`${meetingName} is starting in ${minutesInAdvance} minutes!`,
                     };
                     chrome.notifications.create("remind-notif", notifOptions);
                 }
 
-                chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
-                    if (notifId === "remind-notif") {
-                        if (btnIdx === 0) {
-                            window.open(url);
-                        }
+                /**
+                // why is it opening like 1092302138 windows
+                chrome.notifications.onClicked.addListener(function(notifId) {
+                    if (notifId == "remind-notif") {
+                        console.log('opening meeting');
+                        window.open(url);
                     }
                 });
+                */
                 
                 // if auto join is enabled
                 if (meeting.autojoin && nowTime==schedule[i]) {
